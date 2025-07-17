6a88f82a172498a74d482a7bbba88f0dc44f0b78
Northeast Regional Library <1023nerl@gmail.com>
2025-07-17 14:10:59 +0000

    why is css styling not being applied to the page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,9 @@ import animate from 'tailwindcss-animate';
 const config: Config = {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
