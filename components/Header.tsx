915fdc4ca4a07a760966a86d7a8ba27d2f0e2fde
Northeast Regional Library <1023nerl@gmail.com>
2025-07-18 01:05:59 +0000

    that change messed up our spacing again below the header, undo that cent

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,10 +11,10 @@ interface HeaderProps {
 
 export function Header({ isSimple = false }: HeaderProps) {
   return (
-    <div className={cn("relative", isSimple ? "pt-12" : "pb-24")}>
-      <div className={cn("relative mx-auto max-w-[1200px]")}>
+    <div className={cn("relative pt-12")}>
+      <div className="relative mx-auto max-w-[1200px]">
         {isSimple && (
-          <Link href="/" passHref className="absolute top-[38px] left-0 z-10">
+          <Link href="/" passHref className="absolute top-[34px] left-0 z-10">
             <Button variant="outline" size="icon">
               <Home className="h-5 w-5" />
               <span className="sr-only">Home</span>
@@ -23,19 +23,20 @@ export function Header({ isSimple = false }: HeaderProps) {
         )}
         <div className={cn(
           "bg-card rounded-2xl shadow-lg",
-          isSimple ? "p-6 sm:p-8" : "p-8 sm:p-12",
-          isSimple && "text-center"
+          isSimple ? "p-6 sm:p-8" : "p-8 sm:p-12"
         )}>
-            <a href="/" className="inline-block no-underline">
-              <div className="flex justify-center items-center gap-4 mb-4">
-                  <div className="bg-gradient-to-r from-[#4f46e5] to-[#7c3aed] p-2 rounded-lg">
-                    <Lightbulb className="h-6 w-6 text-accent-foreground" />
-                  </div>
-                  <h1 className="font-sans text-2xl sm:text-3xl md:text-4xl font-bold text-gray-800 dark:text-gray-100">
-                      <span className="title-gradient">Library Launchpad</span>
-                  </h1>
-              </div>
-            </a>
+            <div className={cn("flex items-center", isSimple ? "justify-center" : "")}>
+              <a href="/" className="inline-block no-underline">
+                <div className="flex justify-center items-center gap-4 mb-4">
+                    <div className="bg-gradient-to-r from-[#4f46e5] to-[#7c3aed] p-2 rounded-lg">
+                      <Lightbulb className="h-6 w-6 text-accent-foreground" />
+                    </div>
+                    <h1 className="font-sans text-2xl sm:text-3xl md:text-4xl font-bold text-gray-800 dark:text-gray-100">
+                        <span className="title-gradient">Library Launchpad</span>
+                    </h1>
+                </div>
+              </a>
+            </div>
             {!isSimple && (
               <>
                 <p className="text-base sm:text-lg text-muted-foreground max-w-2xl mx-auto">
