6d8fe23a968c111cc71232489c08df4b02888bb8
Northeast Regional Library <1023nerl@gmail.com>
2025-07-18 22:40:11 +0000

    the width on iphone horizontal still could be wider, see image

--- app/prompting-tips/page.tsx
+++ app/prompting-tips/page.tsx
@@ -9,7 +9,7 @@ export default function PromptingTipsPage() {
 
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-br from-[#667eea] to-[#764ba2]">
-      <div className="container mx-auto max-w-screen-xl flex-grow space-y-8 pt-8">
+      <div className="container mx-auto max-w-screen-xl flex-grow space-y-8 pt-8 px-4 sm:px-8">
         <Header isSimple={true} />
 
         <div className="space-y-8">
@@ -112,3 +112,4 @@ export default function PromptingTipsPage() {
     </main>
   );
 }
+
